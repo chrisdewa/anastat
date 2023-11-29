95bbbc9df6f705825cbe23a357dee1c7c1829709
Chris <christian.delaflor@gmail.com>
2023-11-29 08:18:11 -0600

    Update tools.py
    added clean_corr

--- anastat/tools.py
+++ anastat/tools.py
@@ -1,4 +1,6 @@
 import math
+import pandas as pd
+import numpy as np
 import scipy.stats as stats
 
 def calc_za(alfa: float, decimals=2) -> float:
@@ -13,3 +15,11 @@ def calc_zb(beta: float, decimals=2) -> float:
     returns z score for beta
     """
     return round(stats.norm.ppf(1-beta), decimals)
+
+def clean_corr(df: pd.DataFrame, **kws) -> pd.DataFrame:
+    """
+    returns a cleaned correlation matrix
+        meaning without the first column and last row and without
+        duplicates
+    """
+    return df.corr(**kws).mask(np.tril(np.ones(corr.shape)).astype(bool)).iloc[:-1, 1:]
