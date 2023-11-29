73a6090a8450679b26ad3ce07b04f4556d8957ae
Chris <christian.delaflor@gmail.com>
2023-11-29 10:00:15 -0600

    Update tools.py

--- anastat/tools.py
+++ anastat/tools.py
@@ -25,3 +25,13 @@ def clean_corr(df: pd.DataFrame, **kws) -> pd.DataFrame:
     corr = df.corr(**kws)
     corr = corr.mask(np.tril(np.ones(corr.shape)).astype(bool)).iloc[:-1, 1:]
     return corr
+
+def logit_to_ors(result, alpha=0.05):
+    """
+    result of a statsmodels.api.Logit model fit
+    as an odds ratio dataframe
+    """
+    ORS = result.conf_int(alpha=alpha)
+    ORS['OR'] = result.params
+    ORS = np.exp(ORS)
+    return ORS
